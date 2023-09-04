e5ab95b1985f9c9b284e59f6f3b80e55d0925073
Iyanu-Tomiwa Durotola <durotolaiyanu@gmail.com>
2023-09-03 21:29:50 -0500

    Add folder content

---
+++ src/main/java/com/cs425/elibrary/controller/HomePageController.java
@@ -0,0 +1,12 @@
+package com.cs425.elibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping(value = {"/", "/elibrary", "/elibrary/home"})
+    public String displayHomePage() {
+        return "home/index";
+    }
+}
---
+++ src/test/java/com/cs425/elibrary/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs425.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
